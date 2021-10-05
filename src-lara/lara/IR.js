df10a049208d7c7a9b83e9510367644ea42d3635
Miguel Pedrosa <pedrosa.miguel@gmail.com>
2021-10-05 23:40:12 +0100

    Added code for generating IR

--- src-lara/lara/IR.js
+++ src-lara/lara/IR.js
@@ -1,14 +1,37 @@
-IR.build = function($stmts, extraData) {
-  return {};
+const { decomposeStatment } = Decompose;
+
+IR.build = function ($stmts, extraData) {
+  const IR = new IntermediateRepresentation();
+  $stmts.forEach($stmt => IR.addStatment($stmt, extraData));
+  return IR;
 }
 
-
-IR.updateClava = function(representation) {
+IR.updateClava = function (representation) {
   return;
 }
 
-
-IR.optimize = function(representation) {
-  return {};
+IR.optimize = function (baseRepresentation) {
+  return baseRepresentation;
 }
 
+
+class IntermediateRepresentation {
+  constructor() {
+    // Maps a register's name to its metadata
+    this.statments = [];
+  }
+
+  getRegisterMetadata(name) {
+    return this.registers.get(name);
+  }
+
+  addStatment($stmt) {
+    const stmts = decomposeStatment($stmt);
+    this.statments = [...this.statments, ...stmts];
+  }
+
+  printContents() {
+    for (const stmt of this.statments)
+      println(stmt.toString());
+  }
+}
