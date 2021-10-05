8b8feed61c99aa6cb67d37f500a33fb13d82dc1d
Miguel Pedrosa <pedrosa.miguel@gmail.com>
2021-10-05 17:44:52 +0100

    Added top level api for intermediate representation

---
+++ src-lara/lara/IR.js
@@ -0,0 +1,14 @@
+IR.build = function($stmts, extraData) {
+  return {};
+}
+
+
+IR.updateClava = function(representation) {
+  return;
+}
+
+
+IR.optimize = function(representation) {
+  return {};
+}
+
