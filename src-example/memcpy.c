180b5c76611fd7003c11bc4b037dc52f64d67004
Miguel Pedrosa <pedrosa.miguel@gmail.com>
2021-12-29 12:58:52 +0000

    Reverted example to memcpy

--- src-example/memcpy.c
+++ src-example/memcpy.c
@@ -1,15 +1,20 @@
 #include <stdint.h>
 
 
-void core_kernel(float **path, int N) {
-  #pragma clava data uve : true
-  for (int k = 0; k < N; k++) {
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        path[i][j] = path[i][j] < path[i][k] + path[k][j]
-                         ? path[i][j]
-                         : path[i][k] + path[k][j];
-      }
-    }
+void core_kernel(float *src, float *dest, int sizeN) {
+
+  #pragma clava data kernel.json
+  for (int i = 0; i < sizeN; i++) {
+    dest[i] = src[i];
   }
+
+//   #pragma clava data uve: true
+// // stream u1 -> src[i]
+// // stream u2 -> dest[i]
+//   for (int i = 0; i < sizeN; i++) {
+//     float tmp1, tmp2;
+//     asm volatile("mov x, u1" : x "r" (tmp1));
+//     tmp2 = tmp1;
+//     asm volatile("mov u2, x" : x "r" (tmp2));
+//   }
 }
