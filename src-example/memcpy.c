56e17abb94ba8a9832fc21267cc849e95cf1b677
Miguel Pedrosa <pedrosa.miguel@gmail.com>
2022-01-14 00:03:27 +0000

    Replaced example wiht gemm source code

--- src-example/memcpy.c
+++ src-example/memcpy.c
@@ -1,11 +1,19 @@
 #include <stdint.h>
 
 
-void core_kernel(uint16_t *src, uint16_t*dest, int sizeN) {
+void core_kernel(uint16_t** C, uint16_t** B, uint16_t** A, int sizeI, int sizeJ, int sizeK) {
+  int beta = 23;
+  int alpha = 14;
 
   #pragma clava data kernel.json
-  for (int i = 0; i < sizeN; i++) {
-    dest[i] = src[i];
+  for (int i = 0; i < sizeI; i++) {
+    for (int j = 0; j < sizeJ; j++) {
+      C[i][j] *= beta;
+    }
+    for (int k = 0; k < sizeK; k++) {
+      for (int j = 0; j < sizeJ; j++) {
+        C[i][j] += alpha * A[i][k] * B[k][j];
+      }
+    }
   }
-
 }
