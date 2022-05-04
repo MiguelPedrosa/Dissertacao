6287f4d7b1c7ed08d8e920d9d0541658f45bebfa
MiguelPedrosa <pedrosa.miguel@gmail.com>
2022-05-04 14:17:10 +0100

    renamed execution script

--- Spike-validation/validate.js
+++ Spike-validation/validate.js
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
-
 const { spawnSync }= require("child_process");
 
 
 const kernels = [ "memcpy", "saxpy" ];
+// const kernels = [ "saxpy" ];
+const compileFlags = [ "-Wall", "-pedantic", "-static", "-DTYPE=1" ];
 
-// const compilerPath = "/home/miguel/diss/tools/uve-compiler/bin/riscv64-unknown-linux-gnu-gcc";
-// const pkPath = "/home/miguel/tools/pk-riscv/riscv64-unknown-elf/bin/pk";
-// const spikePath = "/home/miguel/diss/tools/bin/spike";
-
-/* Test veriables while rest of environment isn't ready */
-const spikePath = "echo 'spike';";
-const pkPath = "echo 'pk' && ";
-const compilerPath = "gcc";
-
-const compileFlags = [ "-Wall", "-pedantic", "-static", "-DTYPE=2" ];
+const compilerPath = "/home/miguel/diss/sources/install/uve_tc/bin/riscv64-unknown-elf-gcc";
+const pkPath = "/home/miguel/diss/tools/riscv64-unknown-elf/bin/pk";
+const spikePath = "/home/miguel/diss/tools/bin/spike";
+const bin_simple = "./run_simple";
+const bin_uve = "./run_uve";
 
 
 function aproximateEqual(stdout1, stdout2) {
@@ -26,8 +22,12 @@ function aproximateEqual(stdout1, stdout2) {
   }
 
   for (let i = 0; i < str1.length; i++) {
-    const diff = Math.abs(parseFloat(str1[i]) - parseFloat(str1[i]));
-    if (diff > 0.00001) {
+    const value1 = parseFloat(str1[i]);
+    const value2 = parseFloat(str2[i]);
+
+    const diff = Math.abs(value1 - value2);
+    if (diff > 0.0001) {
+      console.error(`Values were ${str1[i]} and ${str2[i]} with difference of ${diff} at index ${i}`);
       return false;
     }
   }
@@ -35,8 +35,6 @@ function aproximateEqual(stdout1, stdout2) {
   return true;
 }
 
-const bin_simple = "./run_simple";
-const bin_uve = "./run_uve";
 
 for (let kernel of kernels) {
   /* Generate binary without UVE */
@@ -50,19 +48,18 @@ for (let kernel of kernels) {
     break;
   }
   /* Generate binary with UVE */
-  // const comp2 = spawnSync(compilerPath, [...compileFlags, "-DUVE_COMPILATION", `${kernel}.c`, "-o", bin_uve]);
-  const comp2 = spawnSync(compilerPath, [...compileFlags, `${kernel}.c`, "-o", bin_uve]);
+  const comp2 = spawnSync(compilerPath, [...compileFlags, "-DUVE_RUN", `${kernel}.c`, "-o", bin_uve]);
   if (comp2.error) {
-    console.error(`Kernel ${kernel}: An error occured while trying to compile without UVE: ${comp2.error.message}`);
+    console.error(`Kernel ${kernel}: An error occured while trying to compile with UVE: ${comp2.error.message}`);
     break;
   }
   if (comp2.status != 0) {
-    console.error(`Kernel ${kernel}: Compilation without UVE failed: ${comp2.stderr}`);
+    console.error(`Kernel ${kernel}: Compilation with UVE failed: ${comp2.stderr}`);
     break;
   }
 
   /* Run binary without UVE */
-  const exec1 = spawnSync(bin_simple);
+  const exec1 = spawnSync(spikePath, [pkPath, bin_simple]);
   if (exec1.error) {
     console.error(`Kernel ${kernel}: An error occured while trying to run without UVE: ${exec1.error.message}`);
     break;
@@ -72,13 +69,15 @@ for (let kernel of kernels) {
     break;
   }
   /* Run binary with UVE */
-  const exec2 = spawnSync(bin_uve);
+  const exec2 = spawnSync(spikePath, [pkPath, bin_uve]);
   if (exec2.error) {
     console.error(`Kernel ${kernel}: An error occured while trying to run with UVE: ${exec2.error.message}`);
     break;
   }
   if (exec2.status != 0) {
-    console.error(`Kernel ${kernel}: Execution with UVE failed: ${exec2.stderr}`);
+    console.error(`Issue in Kernel ${kernel}: Execution with UVE failed.`)
+    console.error(`Stderr: ${exec2.stderr}`);
+    console.error(`Stdout: ${exec2.stdout}`);
     break;
   }
 
