56917293e660601e0ca2bf63a4f688a28ca3ee78
MiguelPedrosa <pedrosa.miguel@gmail.com>
2022-05-08 00:18:38 +0100

    Moved test kernels to seperate directory

---
+++ Clava-validation/src/memcpy_kernel.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "Commun.h"
+
+
+#ifdef RUN_UVE
+void core(DataType dest[SIZE], DataType src[SIZE]) {
+	asm volatile(
+		"ss.st.d  u1, %[dest], %[size], %[stride] \t\n"
+		"ss.ld.d  u2,  %[src], %[size], %[stride] \t\n"
+		:: [dest] "r" (dest),
+      [src] "r" (src),
+      [size] "r" (SIZE),
+      [stride] "r" (1));
+	asm volatile(
+		".uve_loop%= : \t\n"
+		"so.v.mv u1, u2, p0 \n\t"
+		"so.b.nc u2, .uve_loop%= \n\t" :::);
+}
+#endif // RUN_UVE
+
+
+#ifdef RUN_SIMPLE
+void core(DataType dest[SIZE], DataType src[SIZE]) {
+  for (int i = 0; i < SIZE; i++) {
+    dest[i] = src[i];
+  }
+}
+#endif // RUN_SIMPLE
+
+#ifdef RUN_BLANK
+void core(DataType dest[SIZE], DataType src[SIZE]) {
+
+}
+#endif // RUN_BLANK
+
---
+++ Clava-validation/src/memcpy_main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "Commun.h"
+
+extern void core(DataType dest[SIZE], DataType src[SIZE]);
+
+int main()
+{
+  DataType src[SIZE];
+  DataType dest[SIZE];
+
+  initArray(src);
+  initZero(dest);
+
+  core(dest, src);
+
+  for (int i = 0; i < SIZE; i++)
+    printf( DataFormat("", "\n"), dest[i]);
+}
