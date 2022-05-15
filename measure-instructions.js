c53f2e567e26fcb423385aef60c8f67ddfc79b1b
MiguelPedrosa <pedrosa.miguel@gmail.com>
2022-05-15 23:19:52 +0100

    Fixed issue with compiler argument not being correctly passed to clava

--- measure-instructions.js
+++ measure-instructions.js
@@ -2,8 +2,9 @@
 
 const { spawnSync }= require("child_process");
 
-const kernels = [ "memcpy", "saxpy", "test" ];
-const compileFlags = [ "-Wall", "-pedantic", "-DTYPE=1", "-DSIZE=2048" ];
+// const kernels = [ "3mm", "floyd-marshall", "gemm", "jacobi-1d", "jacobi-2d", "memcpy", "saxpy", "trisolv" ];
+const kernels = [ "test" ];
+const compileFlags = [ "-Wall", "-pedantic", "-DTYPE=1", "-DSIZE=64" ];
 const linkFlags = [ "-Wall", "-pedantic", "-static" ];
 const compilerPath = "/home/miguel/diss/sources/install/uve_tc/bin/riscv64-unknown-elf-gcc";
 const pkPath = "/home/miguel/diss/tools/riscv64-unknown-elf/bin/pk";
@@ -20,7 +21,7 @@ function executableRun(command, args) {
     throw new Error(`An error occured while trying to run ${command}: ${executable.error.message}`);
   }
   if (executable.status != 0) {
-    throw new Error(`Issue in Kernel ${command}: Execution failed\nStderr: ${executable.stderr}\nStdout: ${executable.stdout}`);
+    throw new Error(`Issue in executable ${command} [${args.join(' ')}]: Execution failed\nStderr: ${executable.stderr}\nStdout: ${executable.stdout}`);
   }
   return executable;
 }
@@ -69,12 +70,12 @@ for (let kernel of kernels) {
   /* Compile and link each kernel file */
   compileKernel(compilerPath, [...compileFlags, "-DRUN_BLANK", "-Ibenchmarks/", "-O0", `benchmarks/${kernel}/kernel.c`, "-c" ]);
   compileKernel(compilerPath, [...linkFlags, "-O0", "Commun.o", `kernel.o`, `main.o`, "-o", bin_blank]);
-
+  
   compileKernel(compilerPath, [...compileFlags, "-DRUN_SIMPLE", "-Ibenchmarks/", "-O0", `benchmarks/${kernel}/kernel.c`, "-c" ]);
   compileKernel(compilerPath, [...linkFlags, "-O0", "Commun.o", `kernel.o`, `main.o`, "-o", bin_simple]);
-  
-  executableRun("clava", ["Transform.lara", "-i", "src-lara", `--argv=kernelName:'${kernel}'`, bin_clava]);
-  compileKernel(compilerPath, [...compileFlags, "-DRUN_CLAVA", "-Ibenchmarks/", "-O0", `output/${kernel}/kernel.c`, "-c" ]);
+
+  executableRun("clava", ["Transform.lara", "-i", "src-lara", `--argv=kernelName:'${kernel}', compFlags:'${compileFlags.join(' ')}'`]);
+  compileKernel(compilerPath, [...compileFlags, "-Ibenchmarks/", "-O0", `output/${kernel}/kernel.c`, "-c" ]);
   compileKernel(compilerPath, [...linkFlags, "-O0", "Commun.o", `kernel.o`, `main.o`, "-o", bin_clava]);
 
   /* Run each kernel file */
@@ -96,7 +97,7 @@ for (let kernel of kernels) {
   instructions[key] = processInstructions(blank, simple, clava);
 
   /* Delete executables for next kernel */
-  const del = spawnSync("rm", ["-f", bin_blank, bin_simple, bin_clava, `main.o`, `kernel.o`, `Commun.o`]);
+  const del = spawnSync("rm", ["-rf", bin_simple, bin_clava, 'woven_code', `main.o`, `kernel.o`, `Commun.o`]);
   if (del.error) {
     console.error(`Kernel ${kernel}: An error occured while deleting files for next execution: ${del.error.message}`);
     break;
