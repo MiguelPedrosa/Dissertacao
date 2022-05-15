12c579ad506399da322a17877cc5cbe79e387332
MiguelPedrosa <pedrosa.miguel@gmail.com>
2022-05-15 16:58:57 +0100

    Fixed issues where error file wasn't deleted if empty

--- run-Benchmarks.sh
+++ run-Benchmarks.sh
@@ -6,7 +6,7 @@ ERR_FILE="err.txt"
 ${CMD} 2> "$ERR_FILE"
 
 # If error file is empty, delete it
-[[ -s "$ERR_FILE" ]] && rm "$ERR_FILE"
+[[ ! -s "$ERR_FILE" ]] && rm "$ERR_FILE"
 
 # Removed temporary folder
 rm -rf "woven_code"
